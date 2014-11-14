adde43a26a30bd16d0f7a5c7937667434519500d
MarkoCuk <marko@onyx.si>
2014-11-14 14:34:10 +0100

    database changes

--- Vseved/SchoolDatabase.Designer.cs
+++ Vseved/SchoolDatabase.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Work\Vseved\Vseved\SchoolDatabase.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Work\Repos\VsevedGitHub\Vseved\SchoolDatabase.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
